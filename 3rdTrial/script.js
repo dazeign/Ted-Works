3dd1a6a1f164630bbf3bc1fa6a7f535d420a8adf
Frankpeter Ani <frankpeterani@gmail.com>
2020-04-03 19:30:48 +0100

    I dont even know

--- 3rdTrial/script.js
+++ 3rdTrial/script.js
@@ -11,3 +11,12 @@ function allowDrop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
   }
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+  
+  function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+  }
